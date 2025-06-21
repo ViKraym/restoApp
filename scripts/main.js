f5ec7aebc4133d4459dd5d9cf72fba5f9b0619c4
Viktor Ivanov <viktor.ivan0v98@yandex.ru>
2025-06-21 21:25:53 +0300

    implemented the closure of the delivery notification

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
-const element = document.getElementsByClassName('close-delivery-box');
+  const hideButton = document.getElementById('hideButton');
+  const myBlock = document.getElementById('delivery');
 
-    
+  // hideButton.style.display = "none";
 
-    button.addEventListener('click', function() {
-    element.style.display = "none";
+  hideButton.addEventListener('click', function() {
+    // alert ("button click");
+    if (myBlock.style.display != 'none'){
+      myBlock.style.opacity = '0';
+      myBlock.style.transition = '.3s';
+        let timerId = setTimeout (() => {
+     myBlock.style.display = 'none';
+  }, 300);
+      
+         } else {
+          myBlock.style.display = "flex";
+         }
   });
+
